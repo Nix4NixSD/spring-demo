e6763bcb44638e82b1b58b3d88728e7650fd4891
Jim <jvw91@msn.com>
2023-09-24 18:21:04 +0200

    [Project/Testing] Added transaction test data. Added some doc to Java2ApplicationTests with some tests we could implement.

--- src/main/java/com/nix4nix/bankaccount/utility/StartUpRunner.java
+++ src/main/java/com/nix4nix/bankaccount/utility/StartUpRunner.java
@@ -2,6 +2,7 @@ package com.nix4nix.bankaccount.utility;
 
 import com.nix4nix.bankaccount.entity.Account;
 import com.nix4nix.bankaccount.entity.Customer;
+import com.nix4nix.bankaccount.entity.Transaction;
 import com.nix4nix.bankaccount.repository.AccountRepository;
 import com.nix4nix.bankaccount.repository.CustomerRepository;
 import com.nix4nix.bankaccount.repository.TransactionRepository;
@@ -32,6 +33,7 @@ public class StartUpRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         addCustomerData();
         addAccountData();
+        addTransactionData();
     }
 
     //Adds the customer data using the CustomerRepository
@@ -43,7 +45,7 @@ public class StartUpRunner implements CommandLineRunner {
 
     //Adds the account data using the AccountRepository
     private void addAccountData() {
-        BigDecimal balance = new BigDecimal("1500.05");
+        BigDecimal balance = new BigDecimal("1500.00");
         Customer owner = customerRepository.findById(1L).get(); // getById() is deprecated and this is the (not deprecated) workaround.
         accountRepository.save(new Account("NL99SPDB0123456789", balance, Account.AccountTypes.BETAALREKENING, LocalDateTime.now(), owner));
 
@@ -51,8 +53,25 @@ public class StartUpRunner implements CommandLineRunner {
         owner = customerRepository.findById(2L).get();
         accountRepository.save(new Account("NL99SPDB0987654321", balance, Account.AccountTypes.BETAALREKENING, LocalDateTime.now(), owner));
 
-        balance = new BigDecimal("123.23");
+        balance = new BigDecimal("123.25");
         owner = customerRepository.findById(3L).get();
         accountRepository.save(new Account("NL99SPDB0001237465", balance, Account.AccountTypes.BETAALREKENING, LocalDateTime.now(), owner));
+    }
+
+    private void addTransactionData() {
+        Account account = accountRepository.findById(1L).get();
+        BigDecimal amount = new BigDecimal("1500.00");
+        BigDecimal balance = new BigDecimal("0.00");
+        transactionRepository.save(new Transaction(Transaction.TransactionTypes.ADD, amount, balance, LocalDateTime.now(), "First deposit", account));
+
+        account = accountRepository.findById(2L).get();
+        amount = new BigDecimal("2345.65");
+        balance = new BigDecimal("0.00");
+        transactionRepository.save(new Transaction(Transaction.TransactionTypes.ADD, amount, balance, LocalDateTime.now(), "First deposit", account));
+
+        account = accountRepository.findById(3L).get();
+        amount = new BigDecimal("123.25");
+        balance = new BigDecimal("0.00");
+        transactionRepository.save(new Transaction(Transaction.TransactionTypes.ADD, amount, balance, LocalDateTime.now(), "First deposit", account));
     }
 }
--- src/test/java/com/nix4nix/bankaccount/Java2ApplicationTests.java
+++ src/test/java/com/nix4nix/bankaccount/Java2ApplicationTests.java
@@ -3,11 +3,22 @@ package com.nix4nix.bankaccount;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*
+TODO:
+- test entities and their annotations (unique/not null) by creating a entity with faulty data
+- test retrieval of customer data
+- test retrieval of account data
+- test retrieval of transaction data
+- test transaction rollback
+ */
+
+
 @SpringBootTest
 class Java2ApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
