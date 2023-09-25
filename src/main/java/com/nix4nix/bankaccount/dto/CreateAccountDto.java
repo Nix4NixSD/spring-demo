c4da23fbc5201caa8fd117cef247c9bc7981022b
Jim <jvw91@msn.com>
2023-09-25 23:19:17 +0200

    [AccountController] #2 Added logic for the creation of an account where the endpoint accepts the CreateAccountDto. Changed the CreateAccountDto so it also contains a accountType since we added that to the DTO and the entity some time ago. Sticking to the plan and all.
    Not according to the actual spec given but since it is allowed to somewhat freestyle this i thought this would be oke. In a real world example you should confirm the change of a spec.

--- src/main/java/com/nix4nix/bankaccount/dto/CreateAccountDto.java
+++ src/main/java/com/nix4nix/bankaccount/dto/CreateAccountDto.java
@@ -7,5 +7,6 @@ import java.math.BigDecimal;
 @Data
 public class CreateAccountDto {
     private Long customerId;
+    private String accountType;
     private BigDecimal initialCredit;
 }
--- src/main/java/com/nix4nix/bankaccount/endpoint/AccountController.java
+++ src/main/java/com/nix4nix/bankaccount/endpoint/AccountController.java
@@ -3,12 +3,17 @@ package com.nix4nix.bankaccount.endpoint;
 import com.nix4nix.bankaccount.controlleradvice.exception.NotImplementedException;
 import com.nix4nix.bankaccount.dto.AccountDto;
 import com.nix4nix.bankaccount.dto.CreateAccountDto;
+import com.nix4nix.bankaccount.dto.TransactionDto;
+import com.nix4nix.bankaccount.entity.Transaction;
 import com.nix4nix.bankaccount.service.AccountService;
+import com.nix4nix.bankaccount.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 @RestController
@@ -19,9 +24,37 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
+    @Autowired
+    private TransactionService transactionService;
+
     @PostMapping("/create")
     public ResponseEntity<AccountDto> createAccount(@RequestBody CreateAccountDto createAccountDto) {
-        throw new NotImplementedException(BASE_URL + "/create");
+        AccountDto accountDto = new AccountDto();
+        accountDto.setOwnerId(createAccountDto.getCustomerId());
+        accountDto.setType(createAccountDto.getAccountType());
+
+        // Check if the initialCredit is greater than zero because then we need to initiate a transaction
+        if (createAccountDto.getInitialCredit().compareTo(BigDecimal.ZERO) > 0) {
+            /*
+            Due to the time restrain on this assignment i will just create a transaction and update the account.
+            In a real world example this should be an actual database transaction where you can rollback if one of
+            the prerequisites does not match.
+
+            Maybe create a class "TransactionResolver" that is responsible for handling those transactions. Where checks
+            will be done and the addition/subtraction of the amounts from/to the accounts.
+             */
+            TransactionDto transactionDto = new TransactionDto();
+            transactionDto.setType(String.valueOf(Transaction.TransactionTypes.ADD));
+            transactionDto.setCreatedAt(LocalDateTime.now());
+            transactionDto.setBalance(accountDto.getBalance()); //The balance on the moment of transaction
+            transactionDto.setAmount(createAccountDto.getInitialCredit());
+            transactionService.create(transactionDto);
+
+            accountDto.setBalance(createAccountDto.getInitialCredit());
+        }
+
+        AccountDto result = accountService.create(accountDto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("/get")
